6c9627eaa599a407a9f55734b106c32bf772191e
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2022-01-25 02:06:10 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"Golang-REST/handlers"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handlers.IndexHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
